0da55877774c8d3639332b869469effa36547ec1
MLuminary <418037025@qq.com>
2018-01-15 22:49:19 +0800

    项目准备

---
+++ build/check-versions.js
@@ -0,0 +1,54 @@
+'use strict'
+const chalk = require('chalk')//设置shell输出文案的颜色
+const semver = require('semver')//版本检查工具
+const packageConfig = require('../package.json')
+const shell = require('shelljs')
+
+function exec (cmd) {
+  return require('child_process').execSync(cmd).toString().trim()
+}
+
+const versionRequirements = [
+  {
+    name: 'node',
+    currentVersion: semver.clean(process.version),
+    versionRequirement: packageConfig.engines.node
+  }
+]
+
+if (shell.which('npm')) {
+  versionRequirements.push({
+    name: 'npm',
+    currentVersion: exec('npm --version'),
+    versionRequirement: packageConfig.engines.npm
+  })
+}
+//检查依赖，发出警告和错误
+module.exports = function () {
+  const warnings = []
+
+  for (let i = 0; i < versionRequirements.length; i++) {
+    const mod = versionRequirements[i]
+
+    if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
+      warnings.push(mod.name + ': ' +
+        chalk.red(mod.currentVersion) + ' should be ' +
+        chalk.green(mod.versionRequirement)
+      )
+    }
+  }
+
+  if (warnings.length) {
+    console.log('')
+    console.log(chalk.yellow('To use this template, you must update following to modules:'))
+    console.log()
+
+    for (let i = 0; i < warnings.length; i++) {
+      const warning = warnings[i]
+      console.log('  ' + warning)
+    }
+
+    console.log()
+    process.exit(1)
+  }
+}
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+  <div>
+    这是商品列表
+    <span>{{$route.params.goodsId}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+
