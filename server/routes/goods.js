39510f49ef242659064f7efd89de466435fe7a3f
MLuminary <418037025@qq.com>
2018-03-15 14:23:49 +0800

    分离价格过滤功能

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -24,22 +24,11 @@ router.get("/",function(req,res,next){
   let pageSize = parseInt(req.param("pageSize"));//一页有多少个内容
   let sort = req.param("sort");//正序(1)或升序(-1)
   let skip = (page-1) * pageSize;//跳过文档的个数
-  let priceLevel = req.param("priceLevel");// 价格过滤
-  let priceGt = 0, priceLte = 0; // 价格区间
-  if(priceLevel !== 'all') {
-    switch(priceLevel) {
-      case '0':priceGt = 0   ; priceLte = 100 ;break;
-      case '1':priceGt = 100 ; priceLte = 500 ;break;
-      case '2':priceGt = 500 ; priceLte = 1000;break;
-      case '3':priceGt = 1000; priceLte = 5000;break;
-    }
-    params = {
-      salePrice: {
-        $gt :priceGt,
-        $lte:priceLte
-      }
-    }
-  }
+
+  // 价格过滤
+  let priceLevel = req.param("priceLevel");
+  params = checkPriceArea(priceLevel)
+
   let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
   goodsModel.sort({'salePrice':sort});
   goodsModel.exec({},function(err,doc){
@@ -60,6 +49,7 @@ router.get("/",function(req,res,next){
     }
   })
 })
+
 
 //加入到购物车 直接就为二级路由
 router.post("/addCart",function(req,res,next){
@@ -111,5 +101,27 @@ router.post("/addCart",function(req,res,next){
     }
   })
 });
+
+
+// 返回对应的价格区间
+function checkPriceArea(priceLevel){
+  let params = {};
+  let priceGt = 0, priceLte = 0; // 价格区间
+  if(priceLevel !== 'all') {
+    switch(priceLevel) {
+      case '0':priceGt = 0   ; priceLte = 100 ;break;
+      case '1':priceGt = 100 ; priceLte = 500 ;break;
+      case '2':priceGt = 500 ; priceLte = 1000;break;
+      case '3':priceGt = 1000; priceLte = 5000;break;
+    }
+    params = {
+      salePrice: {
+        $gt :priceGt,
+        $lte:priceLte
+      }
+    }
+  }
+  return params;
+}
 
 module.exports = router;
